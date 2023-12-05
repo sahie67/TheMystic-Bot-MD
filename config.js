9a3427db5d8ea9de861bc4727fa0df455650bbd3
sahie67 <152990306+sahie67@users.noreply.github.com>
2023-12-05 09:19:21 -0500

    updatesahir

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "51962495465"
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['51962495465', '👑 Mystic - Creador 👑', true],
   ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
